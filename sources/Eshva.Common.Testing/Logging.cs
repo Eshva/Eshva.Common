7352f242f5bb0b10e4f9a368867ab70d9ff4fe73
Mike Eshva <reg@eshva.ru>
2021-02-24 00:07:33 +0300

    code transferred from Eshva.Poezd

---
+++ sources/Eshva.Common.Testing/Logging.cs
@@ -0,0 +1,33 @@
+#region Usings
+
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.InMemory;
+using SimpleInjector;
+using Xunit.Abstractions;
+
+#endregion
+
+namespace Eshva.Common.Testing
+{
+  public static class Logging
+  {
+    public static Container AddLogging(this Container container, ITestOutputHelper testOutput)
+    {
+      container.RegisterInstance(GetLoggerFactory(testOutput));
+      container.Register(
+        typeof(ILogger<>),
+        typeof(Logger<>),
+        Lifestyle.Singleton);
+      return container;
+    }
+
+    private static ILoggerFactory GetLoggerFactory(ITestOutputHelper testOutput) =>
+      new LoggerFactory().AddSerilog(
+        new LoggerConfiguration()
+          .WriteTo.InMemory()
+          .WriteTo.TestOutput(testOutput)
+          .MinimumLevel.Verbose()
+          .CreateLogger());
+  }
+}
